a43606ac1d6c3b8ca0642ad07a8b7ec70f59f0e5
NikitaKhapare <NikitaKhapare53@gmail.com>
2022-01-20 18:35:08 -0500

    Rolling Dice

---
+++ DiceRoll.java
@@ -0,0 +1,14 @@
+package com.snake;
+
+public class DiesRoll {
+
+	public static void main(String[] args) {
+		//constants
+		int max=6,min=1;
+		int dice_roll=(int) (Math.random()*max-min+1)+min;
+		System.out.println("Player Rolling Die gives: " +dice_roll);
+		
+
+	}
+
+}
