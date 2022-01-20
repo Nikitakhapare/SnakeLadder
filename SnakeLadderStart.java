bea9cd3d6c7539908858520b851434b33eb5e1aa
NikitaKhapare <NikitaKhapare53@gmail.com>
2022-01-20 18:41:23 -0500

    Snake Ladder start with possition 0

---
+++ SnakeLadderStart.java
@@ -0,0 +1,13 @@
+package com.snake;
+
+public class SnakeLadderStart {
+
+	public static void main(String[] args) {
+		//constants
+		int position=0;
+		System.out.println("Single Player in");
+		System.out.println("Game start with position "+position);
+
+	}
+
+}
