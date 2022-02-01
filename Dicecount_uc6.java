9228d30af3c5919939bfc9e96da8b27b670aa186
Nikita <nikitakhapare53@gmail.com>
2022-02-01 19:16:54 +0530

    Updated indentation

--- Dicecount_uc6.java
+++ Dicecount_uc6.java
@@ -2,17 +2,17 @@ package com.snake;
 
 public class Dicecount_uc6 {
 
-		public static final int Ladder = 1;
+	    public static final int Ladder = 1;
 	    public static final int Snake = 2;
 
 	    public static void main(String[] args) {
 	        //constants
 	        int position = 0;
-	        int Win_Position = 100;
+	        int wiPosition = 100;
 	        int counter=0;
 	        System.out.println("Welcome Player number 1");
 	        //Loop Execute until we rich to the win position
-	        while (position < Win_Position) {
+	        while (position < winPosition) {
 
 	            int dice_roll = (int) ((Math.random() * 10) % 6 + 1);
 	            counter++;
@@ -23,7 +23,7 @@ public class Dicecount_uc6 {
 	            switch (option) {
 	                case Ladder:
 	                    position += dice_roll;
-	                    if (position > Win_Position)
+	                    if (position > winPosition)
 	                        position -= dice_roll;
 	                    System.out.println("Current Position= "+position);
 	                    break;
@@ -39,7 +39,7 @@ public class Dicecount_uc6 {
 
 	        }
 	        System.out.println("number of time dice roll= "+counter);
-	        if (position == Win_Position)
+	        if (position == winPosition)
 	            System.out.println("yeahh win");
 	    }
 
--- ExactWinPosition.java
+++ ExactWinPosition.java
@@ -4,27 +4,27 @@ public class ExactWinPosition {
 
 	public static final int Ladder=1;
 	public static final int Snake = 2;
-    public static void main(String[] args) {
+        public static void main(String[] args) {
 		//constants
-	    int position=0; 
-		int Win_Position=100;
+	        int position=0; 
+		int winPosition=100;
 		System.out.println("Welcome Player number 1");
 		//Loop Execute until we rich to the win position
-	    while(position != Win_Position)
-	  {
-		int dice_roll=(int) ((Math.random() * 10) % 6 +1);
+	        while(position != Win_Position)
+	           {
+		      int diceRoll=(int) ((Math.random() * 10) % 6 +1);
 		
-		//For checking whether there is ladder or snake
-        int option=(int)((Math.random() * 10) % 3);	
-		switch(option) 
-		{
-		case Ladder : position+=dice_roll;
-		              
-		              break;
-		case Snake  : position-=dice_roll;
-		              break;
-		    default : 
-		 }
+		      //For checking whether there is ladder or snake
+                        int option=(int)((Math.random() * 10) % 3);	
+		    switch(option) 
+		      {
+		         case Ladder : position+=dice_roll;
+		                       break;
+
+		         case Snake  : position-=dice_roll;
+  		                       break;
+		             default : 
+		      }
 		
 
     	}			
