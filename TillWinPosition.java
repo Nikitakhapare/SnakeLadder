35c2a0f92013866f47e05cb81496783c8e72094c
Nikita <nikitakhapare53@gmail.com>
2022-02-01 19:28:00 +0530

    Updated Intendation

--- TillWinPosition.java
+++ TillWinPosition.java
@@ -3,38 +3,40 @@ package com.snake;
 public abstract class TillWinPosition {
 
 	public static final int Ladder = 1;
-    public static final int Snake = 2;
+        public static final int Snake = 2;
 
-    public static void main(String[] args) {
-        //constants
-        int position = 0;
-        int Win_Position = 100;
-        System.out.println("Welcome Player number 1");
-        //Loop Execute until we rich to the win position
-        while (position < Win_Position) {
+        public static void main(String[] args) 
+        {
+           //constants
+           int position = 0;
+           int winPosition = 100;
+           System.out.println("Welcome Player number 1");
 
-            int dice_roll = (int) ((Math.random() * 10) % 6 + 1);
+           //Loop Execute until we rich to the win position
+           while (position < Win_Position)
+             {
+                int dice_roll = (int) ((Math.random() * 10) % 6 + 1);
 
-            //For checking whether there is ladder or snake
-            int option = (int) (Math.floor(Math.random() * 10) % 3);
-            switch (option) {
-                case Ladder:
-                    position += dice_roll;
-                    if (position > Win_Position)
-                        position -= dice_roll;
-                    break;
+            	//For checking whether there is ladder or snake
+           	 int option = (int) (Math.floor(Math.random() * 10) % 3);
+                 switch (option) {
+     	           case Ladder:
+                 	       position += diceRoll;
+                               if (position > winPosition)
+                  	       position -= diceRoll;
+                               break;
 
-                case Snake:
-                    position -= dice_roll;
-                    break;
+                   case Snake:
+                   	       position -= diceRoll;
+                	       break;
 
-                default:
+                      default:
 
-            }
+                  }
 
-        }
-        if (position == Win_Position)
-            System.out.println("yeahh win");
+              }
+               if (position == winPosition)
+               System.out.println("yeahh win");
     }
 
   }
