5fc149c658eec09bea8248e83b72d0f6aefd2817
Nikita <nikitakhapare53@gmail.com>
2022-02-01 19:23:13 +0530

    Updated Intendation

--- StartSnakeladder.java
+++ StartSnakeladder.java
@@ -4,19 +4,19 @@ public class StartSnakeladder {
 
 	public static final int Ladder=1;
 	public static final int Snake = 0;
-    public static void main(String[] args) {
+        public static void main(String[] args) {
 		//constants
 		int position=0;
-	    int max=6,min=1;
-		int dice_roll=(int) (Math.random()*max-min+1)+min;
-		System.out.println("Player Rolling Die gives: " +dice_roll);
+	        int max=6,min=1;
+		int diceRoll=(int) (Math.random()*max-min+1)+min;
+		System.out.println("Player Rolling Die gives: " +diceRoll);
 		int option=(int)(Math.random()*10)%3;	
 		System.out.println("Player check for option "+option );
 		switch(option) {
 		
-		case Ladder : position+=dice_roll;
+		case Ladder : position+=diceRoll;
 		              break;
-		case Snake  : position-=dice_roll;
+		case Snake  : position-=diceRoll;
 		             break;
 		    default :
                      
